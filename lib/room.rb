ebbd3aabdf0f77a8ab223fd0ec2a639527498f2b
Tylor Schafer <tylorschafer@gmail.com>
2019-07-03 09:13:03 -0600

    Add room class functionality

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,15 @@
 class Room
 
+  attr_reader :category
+
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+  end
+
+  def area
+    @length * @width
+  end
+
 end
