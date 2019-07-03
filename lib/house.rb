2b9475267f9abcabf0125f73190965ee0a05b342
Tylor Schafer <tylorschafer@gmail.com>
2019-07-03 09:52:11 -0600

    Add itr_4 house tests

--- lib/house.rb
+++ lib/house.rb
@@ -11,6 +11,13 @@ class House
     @rooms << room
   end
 
+  def rooms_from_category(select)
+    @rooms.select{|room| room.category == select}
+  end
+
+  def area
+    @rooms.map{|room| room.area}.sum
+  end
 
 
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -35,7 +35,7 @@ def test_add_rooms_gets_inserted_to_rooms_array
   assert [@room_1, @room_2], @house.rooms
 end
 
-def test_rooms_from_category_method
+def test_rooms_from_category
   @house.add_room(@room_1)
   @house.add_room(@room_2)
   @house.add_room(@room_3)
@@ -44,12 +44,29 @@ def test_rooms_from_category_method
   assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom)
 end
 
-def test_total_house_area_method
+def test_total_house_area
   @house.add_room(@room_1)
   @house.add_room(@room_2)
   @house.add_room(@room_3)
   @house.add_room(@room_4)
   assert_equal 1900, @house.area
+end
+
+def test_price_per_square_foot
+  @house.add_room(@room_1)
+  @house.add_room(@room_2)
+  @house.add_room(@room_3)
+  @house.add_room(@room_4)
+  assert_equal 210.53, @house.test_price_per_square_foot
+end
+
+def test_rooms_by_category
+  skip
+  @house.add_room(@room_1)
+  @house.add_room(@room_2)
+  @house.add_room(@room_3)
+  @house.add_room(@room_4)
+  assert_equal :bedroom=>[@room_1, @room_2], @house.rooms_by_category
 end
 
 
