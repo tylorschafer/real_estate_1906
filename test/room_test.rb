26157e31cd3fc8f755a3e547a944c1c2ec193df2
Tylor Schafer <tylorschafer@gmail.com>
2019-07-03 09:10:16 -0600

    Add room tests

--- test/room_test.rb
+++ test/room_test.rb
@@ -4,4 +4,20 @@ require './lib/room'
 
 class RoomTest < Minitest::Test
 
+def setup
+  @room = Room.new(:bedroom, 10, 13)
+end
+
+def test_room_is_room_class
+  assert_instance_of Room, @room
+end
+
+def test_room_category
+  assert_equal :bedroom, @room.category
+end
+
+def test_room_area
+  assert_equal 130, @room.area
+end
+
 end
